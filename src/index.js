65179a8f34646c03d8d25f2a838c7ca353a12d34
Daniel-Kigozi <kdanalex69@gmail.com>
2022-12-23 10:59:42 +0300

    deleted unused code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import './style.css';
 import { TodoList } from './modules/addandremove.js';
 
-// import {
-//   TodoList,
-// } from './modules/addandremove.js';
+
 
 const todoList = new TodoList();
 todoList.init();
-
-// inputs();
-// render(todos);
-// clearAll();
-// editTodo(document);
-
-// // event delegation
-// document.addEventListener('click', (e) => {
-//   removeElement(e.target);
-//   checkedComplete(e.target);
-// });
