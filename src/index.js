889f27663c4a222d32dc0a43ef43e176e7bb5b1b
Daniel-Kigozi <kdanalex69@gmail.com>
2022-12-23 11:04:53 +0300

    fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './style.css';
-import { TodoList } from './modules/addandremove.js';
-
-
+import TodoList from './modules/todofunction.js';
 
 const todoList = new TodoList();
 todoList.init();
