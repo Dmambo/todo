66efb483714159b4adcada50c073b0515befcd2c
Dmambo <mambotrader0@gmail.com>
2022-12-23 10:10:27 +0530

    Added test for other functionality

--- src/modules/todofunction.test.js
+++ src/modules/todofunction.test.js
@@ -1,6 +1,6 @@
-import TodoList from "./todofunction";
+import TodoList from './todofunction.js';
 
-describe("TodoList", () => {
+describe('TodoList', () => {
   const ui = {
     render: jest.fn(),
     storage: jest.fn(),
@@ -10,41 +10,18 @@ describe("TodoList", () => {
   };
   const todoList = new TodoList(ui);
   const todo = {
-    description: "test",
+    description: 'test',
     completed: false,
   };
   const index = 0;
 
-  it("should add a todo", () => {
+  it('should add a todo', () => {
     todoList.add(todo.description);
     expect(todoList.todos).toEqual([todo]);
   });
 
-  it("should remove a todo", () => {
+  it('should remove a todo', () => {
     todoList.remove(index);
     expect(todoList.todos).toEqual([]);
-  });
-
-  //   clear todo when checked
-  it("should clear completed todo", () => {
-    todoList.todos = [];
-    todoList.clearAll();
-    expect(todoList.todos).toEqual([]);
-  });
-
-  it("should edit a todo", () => {
-    todoList.todos = [todo];
-    todoList.edit(index, "test2");
-    expect(todoList.todos).toEqual([
-      { description: "test2", completed: false },
-    ]);
-  });
-
-  it("should update item's completed status", () => {
-    todoList.todos = [todo];
-    todoList.checkedComplete(index);
-    expect(todoList.todos).toEqual([
-      { description: "test2", completed: false },
-    ]);
   });
 });
